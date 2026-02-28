f80d46eb48a8a3e0e92646cd45e5a8a6dc438103
Yahia Loay <helpyahia12231@gmail.com>
2026-02-28 09:16:30 +0000

    removed search options

--- neovim/init.lua
+++ neovim/init.lua
@@ -5,13 +5,6 @@ vim.opt.clipboard = "unnamedplus"	-- copy to the system clipboard (globally)
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 
--- Searching
-vim.opt.incsearch = true            -- search as characters are entered
-vim.opt.hlsearch = false            -- do not highlight matches
-vim.opt.ignorecase = true           -- ignore case in searches by default
-vim.opt.smartcase = true            -- but make it case sensitive if an uppercase is entered
-
-
 -- lazy.nvim plugin manager
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
